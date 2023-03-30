02a76cd26ad539e6a7f80f573212febb9a0a2733
kartik800 <54595695+kartik800@users.noreply.github.com>
2023-03-30 18:47:20 +0530

    Create 66. Plus One.cpp

---
+++ 66. Plus One.cpp
@@ -0,0 +1,22 @@
+// problem link: https://leetcode.com/problems/plus-one/description/
+// Kartik kumar
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n= digits.size();
+
+        for(int i=n-1;i>=0;i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }else{
+                digits[i] = 0;
+            }
+        }
+        digits.push_back(0);
+        digits[0] = 1;
+        return digits;
+        
+    }
+};
